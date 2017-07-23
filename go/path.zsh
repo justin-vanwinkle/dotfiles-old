fb172eac839ac35e55f8b93253ada36aee59babd
Justin V <vanwinkle.justin@gmail.com>
2017-07-22 21:09:24 -0400

    dropping awesome

--- go/path.zsh
+++
@@ -1,2 +0,0 @@
-export GOPATH="$PROJECTS/go:/Users/vanwinklej/workspace/WA/stateCountrySync:/Users/vanwinklej/workspace/WA/DonationProcessingSystem/GivingWebsiteProcessor"
-export PATH="$GOPATH/bin:$PATH"
